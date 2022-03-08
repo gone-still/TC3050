8e0ea2108f6757f1aa12d5eefed4a990e38e72fd
gone-still <eldesgraciado@gmail.com>
2022-03-08 16:11:40 -0600

    Updated act-03.py

--- act-03 - Adaptive Threshold/act-03.py
+++ act-03 - Adaptive Threshold/act-03.py
@@ -1,13 +1,24 @@
 # File        :   act-03.py (Activity 3 of 2022 Spring Vision Course)
-# Version     :   1.0.1
+# Version     :   1.0.2
 # Description :   Introducing adaptive thresholding
-# Date:       :   Feb 02, 2022
+# Date:       :   Mar 08, 2022
 # Author      :   Ricardo Acevedo-Avila (racevedoaa@gmail.com)
 # License     :   MIT
 
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+
+
+# Read an image:
+def readImage(imagePath):
+    # Loads image:
+    inputImage = cv2.imread(imagePath)
+    # Checks if image was successfully loaded:
+    if inputImage is None:
+        print("readImage>> Error: Could not load Input image.")
+
+    return inputImage
 
 
 # Defines a re-sizable image window:
@@ -29,7 +40,7 @@ path = "D://opencvImages//"
 fileName = "sudokuImage.png"
 
 # Reading an image in default mode:
-inputImage = cv2.imread(path + fileName)
+inputImage = readImage(path + fileName)
 
 # Convert RGB to grayscale:
 grayscaleImage = cv2.cvtColor(inputImage, cv2.COLOR_BGR2GRAY)
@@ -56,7 +67,7 @@ plt.show()
 # Try thresholding the image via Otsu:
 automaticThreshold, binaryImage = cv2.threshold(grayscaleImage, 0, 255, cv2.THRESH_OTSU)
 showImage("binaryImage [Otsu 1]", binaryImage)
-writeImage(path+"sudokuGlobalBinary", binaryImage)
+# writeImage(path+"sudokuGlobalBinary", binaryImage)
 
 # [Post] Try to clean the image applying
 # Gaussian Blur to the grayscale image:
@@ -73,6 +84,7 @@ binaryImage = cv2.adaptiveThreshold(grayscaleImage, 255, cv2.ADAPTIVE_THRESH_GAU
                                     constantValue)
 # Show the result:
 showImage("binaryImage [Adaptive - Gaussian]", binaryImage)
+# writeImage(path+"sudokuAdaptative", binaryImage)
 
 # # apply adaptive thresholding (Mean):
 # windowSize = 7
